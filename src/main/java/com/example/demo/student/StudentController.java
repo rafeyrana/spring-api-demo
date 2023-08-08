bd614460b1260374c857bac0ee36e1174a89e8cf
Rafey Rana <24100103@lums.edu.pk>
2023-08-08 15:22:12 +0500

    DELETE

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,32 +1,39 @@
 package com.example.demo.student;
+
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // here we will keep all the resources here
-@RestController 
+@RestController
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
     private final StudentService studentService;
+
     @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
+
     @GetMapping() // this is the GET method
     public List<Student> getStudents() {
         return studentService.getStudents();
     }
+
     @PostMapping // this is the POST method
     public void addStudent(@RequestBody Student student) { // we take the student from the request body and map it
         studentService.addNewStudent(student);
     }
 
-
-
-
+    @DeleteMapping(path = "{studentId}") // Corrected path // this is the DELETE method
+    public void removeStudent(@PathVariable("studentId") Long studentId) {
+        studentService.removeStudent(studentId);
     }
 
+}
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,4 +1,5 @@
 package com.example.demo.student;
+
 import java.util.Optional;
 import java.util.List;
 
@@ -8,27 +9,36 @@ import org.springframework.stereotype.Service;
 @Service
 public class StudentService {
     private final StudentRepository studentRepository;
+
     @Autowired
     public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
+
     public List<Student> getStudents() { // this is now an api returning an object of the student class
         return studentRepository.findAll();
-        // the benefit of using JPA is that we dont have to implement the functionality for findall etc as it is all available as .functions on the repo
-                                                                                             
+        // the benefit of using JPA is that we dont have to implement the functionality
+        // for findall etc as it is all available as .functions on the repo
+
     }
+
     public void addNewStudent(Student student) {
         System.out.println(student);
-        Optional<Student> studentByEmail=  studentRepository.findStudentByEmail(student.getEmail());
+        Optional<Student> studentByEmail = studentRepository.findStudentByEmail(student.getEmail());
         if (studentByEmail.isPresent()) {
             throw new IllegalStateException("email already exists");
-        }
-        else{
+        } else {
             studentRepository.save(student);
         }
-
-    
         // since this is the service we will be implementing the business logic here
+    }
+
+    public void removeStudent(Long studentId) {
+        boolean valid = studentRepository.existsById(studentId);
+        if (!valid) {
+            throw new IllegalStateException("student for this id does not exist");
+        }
+        studentRepository.deleteById(studentId);
 
     }
 }
